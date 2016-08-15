0636d4b1cdeeabb57899602cc239c5990f4610cc
Shelly Jindal <sjindal@C02PHGACG3QC.local>
2016-08-15 12:28:41 -0700

    adding the latest

--- test/app.js
+++ test/app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-generator').test;
 
-describe('generator-fanatics-components:app', function () {
+describe('react-components-generator:app', function () {
   before(function (done) {
     helpers.run(path.join(__dirname, '../generators/app'))
       .withOptions({someOption: true})
